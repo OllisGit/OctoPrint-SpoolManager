83b8dc688b0c3bbddd85c2ad513d6295eb2de0b1
OllisGit <olaf.kaus+github@gmail.com>
2021-01-25 11:40:48 +0100

    rebuild test2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ plugin_name = "OctoPrint-SpoolManager"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 #
-plugin_version = "1.3.1-rebuild"
+plugin_version = "1.3.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
