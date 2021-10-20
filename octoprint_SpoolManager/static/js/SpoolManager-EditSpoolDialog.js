d10e03ffbdac5577555bf8a30149a07550979a74
Tiziano Gemma <git@tizl.ch>
2021-10-20 15:36:44 +0200

    refactoring: restore density map formatting

--- octoprint_SpoolManager/static/js/SpoolManager-EditSpoolDialog.js
+++ octoprint_SpoolManager/static/js/SpoolManager-EditSpoolDialog.js
@@ -45,23 +45,23 @@ function SpoolManagerEditSpoolDialog() {
     // ITEM MODEL
     var DEFAULT_COLOR = "#ff0000";
     var densityMap = {
-        PLA: 1.24,
-        PLA_plus: 1.24,
-        ABS: 1.04,
-        PETG: 1.27,
-        NYLON: 1.52,
-        TPU: 1.21,
-        PC: 1.3,
-        Wood: 1.28,
-        Carbon: 1.3,
-        PC_ABS: 1.19,
-        HIPS: 1.03,
-        PVA: 1.23,
-        ASA: 1.05,
-        PP: 0.9,
-        POM: 1.4,
-        PMMA: 1.18,
-        FPE: 2.16
+        PLA:	1.24,
+        PLA_plus:	1.24,
+        ABS:	1.04,
+        PETG:	1.27,
+        NYLON:	1.52,
+        TPU:	1.21,
+        PC:	    1.3,
+        Wood:	1.28,
+        Carbon:	1.3,
+        PC_ABS:	1.19,
+        HIPS:	1.03,
+        PVA:	1.23,
+        ASA:	1.05,
+        PP:	    0.9,
+        POM:	1.4,
+        PMMA:	1.18,
+        FPE:	2.16
     };
 
     self.unitValues = {
