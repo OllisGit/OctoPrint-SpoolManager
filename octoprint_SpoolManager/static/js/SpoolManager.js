aa66959b2b4b4bb3ec65c781004249f3d02c1ce7
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2023-04-04 18:39:13 +0200

    GH-1 Remove unused function

--- octoprint_SpoolManager/static/js/SpoolManager.js
+++ octoprint_SpoolManager/static/js/SpoolManager.js
@@ -81,23 +81,6 @@ $(function() {
             }
         };
 
-
-        // found here: https://stackoverflow.com/questions/19491336/how-to-get-url-parameter-using-jquery-or-plain-javascript?rq=1
-        var getUrlParameter = function getUrlParameter(sParam) {
-            var sPageURL = window.location.search.substring(1),
-                sURLVariables = sPageURL.split('&'),
-                sParameterName,
-                i;
-
-            for (i = 0; i < sURLVariables.length; i++) {
-                sParameterName = sURLVariables[i].split('=');
-
-                if (sParameterName[0] === sParam) {
-                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-                }
-            }
-        };
-
         self.reloadQRCodePreviewImage = function(){
             var imageDom = $("#settings-qrimage-preview");
             var currentSrc = imageDom.attr("src");
